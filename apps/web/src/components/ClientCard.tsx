d65deb00993716eaf10f766c139e06d8fa78dbe9
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2025-10-18 18:02:40 -0300

    feat: implementar funções mock e comunicação pai-filho

--- apps/web/src/components/ClientCard.tsx
+++ apps/web/src/components/ClientCard.tsx
@@ -5,10 +5,13 @@ import TrashIcon from "../assets/icons/TrashIcon";
 type ClientCardProps = {
   user: any,
   onClick?: () => void;
+  onViewDetails?: () => void;
+  onEdit?: () => void;
+  onDelete?: () => void;
 };
 
-export default function ClientCard ({ user: {name, salary, companyValuation}, onClick }: ClientCardProps) {
-  return (
+export default function ClientCard ({ user: {name, salary, companyValuation}, onClick, onDelete, onEdit, onViewDetails }: ClientCardProps) {
+   return (
     <div 
       onClick={onClick}
       className="border border-gray-200 rounded-lg p-6 hover:shadow-md transition-shadow cursor-pointer
@@ -20,16 +23,26 @@ export default function ClientCard ({ user: {name, salary, companyValuation}, on
         <p className="text-black">Salário: R$ {salary.toLocaleString()}</p>
         <p className="text-black">Empresa: R$ {companyValuation.toLocaleString()}</p>
       </div>
-      <div className="flex justify-between items-center w-full mt-5"
+      <div 
+        className="flex justify-between items-center w-full mt-5"
+        onClick={(e) => e.stopPropagation()}
       >
-        <button className="cursor-pointer bg-transparent"
+        <button 
+          className="cursor-pointer bg-transparent"
+          onClick={onViewDetails}
         >
           <PlusIcon/>
         </button>
-        <button className="cursor-pointer bg-transparent">
+        <button 
+          className="cursor-pointer bg-transparent"
+          onClick={onEdit}
+          >
           <EditIcon/> 
         </button>
-        <button className="cursor-pointer bg-transparent">
+        <button 
+          className="cursor-pointer bg-transparent"
+          onClick={onDelete}
+          >
           <TrashIcon/>
         </button>
       </div>
--- apps/web/src/pages/Clients.tsx
+++ apps/web/src/pages/Clients.tsx
@@ -2,19 +2,36 @@ import ClientCard from "../components/ClientCard";
 
 
 const mockUser = {
-  id: 1,
+  id: '1',
   name: 'Eduardo',
   salary: 3000, 
   companyValuation: 12000 
 }
 
 export default function Clients(){
+  const handleViewDetails = (userId: string) => {
+  console.log('Ver detalhes do cliente:', userId);
+  };
+
+  const handleEdit = (userId: string) => {
+    console.log('Editar cliente:', userId);
+  };
+
+  const handleDelete = (userId: string) => {
+    console.log('Deletar cliente:', userId);
+  };
+
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-center px-4 bg-brand-background ">
       <div className="w-full max-w-md space-y-8">
         <p>X clientes foram encontrados</p>
         <div className="flex flex-col gap-5">
-          <ClientCard user={mockUser}/>
+          <ClientCard 
+            user={mockUser}
+            onViewDetails={() => handleViewDetails(mockUser.id)}
+            onEdit={() => handleEdit(mockUser.id)}
+            onDelete={() => handleDelete(mockUser.id)}
+          />
         </div>
       </div>
     </main>
