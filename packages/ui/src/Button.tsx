34653ad5313d65b90e9a47c89599976acd5fcc95
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2025-10-18 15:37:34 -0300

    feat: criar componente Button no design system

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,9 +1,14 @@
-
 type ButtonProps = {
   label: string;
   onClick?: () => void;
+  className?: string;
 };
 
-export const Button = ({ label, onClick }: ButtonProps) => (
-  <button onClick={onClick}>{label}</button>
+export const Button = ({ label, onClick,className}: ButtonProps) => (
+  <button 
+    onClick={onClick} 
+    className={`bg-brand-primary h-[60px] max-w-[521px] min-w-[250px] font-bold text-lg text-white
+     ${className}
+  `}
+  >{label}</button>
 );
