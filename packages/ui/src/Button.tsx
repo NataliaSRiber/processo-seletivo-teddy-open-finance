f80acf85ce4041ca78cb4b8a40feef3a0eb34928
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2025-10-18 17:33:33 -0300

    style: diminuir padding vertical do botão

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -7,8 +7,8 @@ type ButtonProps = {
 export const Button = ({ label, onClick, className}: ButtonProps) => (
   <button 
     onClick={onClick} 
-    className={`bg-brand-primary h-[60px] sm:max-w-[400px] md:max-w-[521px] lg:max-w-[521px] font-bold text-white text-xl md:text-2xl rounded-lg           px-4
-      py-4 lg:text-3xl cursor-pointer w-full ${className}
+    className={`bg-brand-primary h-[60px] sm:max-w-[400px] md:max-w-[521px] lg:max-w-[521px] font-bold text-white text-xl md:text-2xl rounded-lg px-4
+      py-2 lg:text-3xl cursor-pointer w-full ${className}
   `}
   >{label}</button>
 );
