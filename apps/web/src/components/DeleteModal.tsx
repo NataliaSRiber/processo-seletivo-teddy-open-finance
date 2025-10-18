b52c341ff9e12b10970764e4f35f3325fb5d28d3
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2025-10-18 20:47:09 -0300

    feat: implementar modal de deleção de cliente

---
+++ apps/web/src/components/DeleteModal.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@teddy/ui";
+import type { User } from "../types/user";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title?: string;
+  user?: User
+} 
+
+export default function DeleteModal({ isOpen, onClose, user}: ModalProps) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-white rounded-lg p-6 w-full max-w-md mx-4">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-xl font-bold text-gray-900">Excluir cliente: </h2>
+          <button
+            onClick={onClose}
+            className="text-gray-500 hover:text-gray-700 text-2xl"
+          >
+            ×
+          </button>
+        </div>
+        <p>Você está prestes a excluir o cliente: {user?.name}</p>
+        <Button label="Excluir cliente" className="mt-5" />
+      </div>
+    </div>
+  );
+}
