46c7784a3f053136320a5f0909dcd60fedfa1ee8
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2025-10-19 09:04:28 -0300

    feat: criar página visual de clientes selecionados

--- apps/web/src/pages/SelectedClient.tsx
+++ apps/web/src/pages/SelectedClient.tsx
@@ -1,8 +1,26 @@
+import ClientCard from "../components/ClientCard";
+
+
+const mockUser = {
+  id: '1',
+  name: 'Eduardo',
+  salary: 3000, 
+  companyValuation: 12000 
+}
+
 export default function SelectedClient(){
 
   return (
-    <main className="mt-20 flex h-full w-full flex-col items-center justify-center bg-newblue-950 py-16 md:mt-44">
-      <h1>Clients details</h1>
+    <main className="min-h-screen w-full px-4 bg-brand-background py-8 mt-20"> 
+      <div className="max-w-md mx-auto space-y-8"> 
+        <p>Clientes selecionados</p>
+        <div className="flex flex-col gap-5">
+          <ClientCard 
+            user={mockUser}
+            isSelected={true}
+          />
+        </div>
+      </div>
     </main>
   )
 }
