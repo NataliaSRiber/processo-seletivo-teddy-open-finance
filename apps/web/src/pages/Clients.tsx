f0134b1ac57cbea47991e62f301522a4860de015
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2025-10-19 10:22:41 -0300

    feat: integrar API para a exibição da listagem de clientes

--- apps/web/src/pages/Clients.tsx
+++ apps/web/src/pages/Clients.tsx
@@ -1,47 +1,68 @@
 import { Button } from "@teddy/ui";
 import ClientCard from "../components/ClientCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "../components/Modal";
 import DeleteModal from "../components/DeleteModal";
-
-const mockUser = {
-  id: '1',
-  name: 'Eduardo',
-  salary: 3000, 
-  companyValuation: 12000 
-}
+import { userService } from "../services/api";
+import type { User } from "../types/user";
+import { toast } from "sonner";
 
 export default function Clients(){
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
-  const [, setClientToDelete] = useState('');
+  const [, setClientToDelete] = useState<number | null>(null);
+  const [users, setUsers] = useState<User[]>([]);
 
-  const handleViewDetails = (userId: string) => {
+  const handleViewDetails = (userId: number) => {
     console.log('Ver detalhes do cliente:', userId);
   };
 
-  const handleEdit = (userId: string) => {
+  const handleEdit = (userId: number) => {
     setIsEditModalOpen(true);
     console.log('Editar cliente:', userId);
   };
 
-  const handleDelete = (userId: string) => {
+  const handleDelete = (userId: number) => {
     setDeleteModalOpen(true);
     setClientToDelete(userId)
     console.log('Deletar cliente:', userId);
   };
 
+  const loadUsers = async () => {
+    try {
+      const response = await userService.getAll();
+      const list = response.data.clients;
+      setUsers(list);
+    } catch (error: any) {
+      toast.error(`Erro ao carregar usuários: ${error.message}`);
+    }
+  };
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+
   return (
     <main className="min-h-screen w-full px-4 bg-brand-background py-8 mt-20"> 
-      <div className="max-w-md mx-auto space-y-8"> 
-        <p>1 cliente foi encontrado</p>
+      <div className="max-w-md mx-auto space-y-8">
         <div className="flex flex-col gap-5">
-          <ClientCard 
-            user={mockUser}
-            onViewDetails={() => handleViewDetails(mockUser.id)}
-            onEdit={() => handleEdit(mockUser.id)}
-            onDelete={() => handleDelete(mockUser.id)}
-          />
+        <p className="mb-5">{users.length === 1 ? ' 1 cliente foi encontrado:': `${users.length} clientes foram encontrados:` }</p>
+          {
+            users.length === 0 ? (
+              <p>Não há clientes cadastrados</p>
+            ) : (
+              users.map((user) => (
+                <ClientCard
+                  key={user.id}
+                  user={user}
+                  onViewDetails={() => user.id && handleViewDetails(user.id)}
+                  onEdit={() => user.id && handleEdit(user.id)}
+                  onDelete={() => user.id && handleDelete(user.id)}
+                />
+              ))
+            )
+          }
           <Modal
             isOpen={isEditModalOpen}
             onClose={() => setIsEditModalOpen(false)}
