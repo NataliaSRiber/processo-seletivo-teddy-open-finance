a0ccae52fabd05271fd17987e5875149d63c993f
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2025-10-19 18:28:59 -0300

    criar página de clientes selecionados

--- apps/web/src/pages/SelectedClient.tsx
+++ apps/web/src/pages/SelectedClient.tsx
@@ -1,25 +1,33 @@
+import { useSelectedClients } from "../context/SelectedClientsContext";
 import ClientCard from "../components/ClientCard";
+import { Button } from "@teddy/ui";
 
-
-const mockUser = {
-  id: '1',
-  name: 'Eduardo',
-  salary: 3000, 
-  companyValuation: 12000 
-}
 
 export default function SelectedClient(){
+  const { selectedUsers, clearAll } = useSelectedClients();
+  
 
   return (
     <main className="min-h-screen w-full px-4 bg-brand-background py-8 mt-20"> 
       <div className="max-w-md mx-auto space-y-8"> 
         <p>Clientes selecionados</p>
         <div className="flex flex-col gap-5">
-          <ClientCard 
-            user={mockUser}
-            isSelected={true}
-          />
+          {
+            selectedUsers.length == 0  ? (
+               <p>Não há clientes selecionados </p>
+            ) : (
+              selectedUsers.map((user) =>  (
+                <ClientCard 
+                  user={user}
+                />
+              ))
+            )
+          }
         </div>
+        <Button
+          label="Limpar clientes selecionados"
+          onClick={clearAll}
+        />
       </div>
     </main>
   )
